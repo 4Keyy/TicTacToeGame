e18fe4df23708d9555de56e5268dc6696abc89de
4keyy <4kpoison@gmail.com>
2021-08-02 13:35:35 +0300

    The first class was created to hold the variable at the start of the game. Also fixed some problems with comments in the MainSource file. A header file has been created that stores a class with functions for starting the game.

---
+++ TicTacToeGame/ConnectingFunctions.h
@@ -0,0 +1,25 @@
+// * This file is used to connect, title and transfer functions between all project files
+
+// This line indicates that this file was connected strictly once.
+#pragma once
+
+// Also for this file you need to add libraries
+#include <iostream> // Standard library for working with C ++ console
+
+// I will store the functions in classes, and call them through the created objects in the main functions, create a class
+// In this class i will keep functions for start the game, all functions use abstraction so there won't be a private line
+class StartGameFunctions
+{
+public:
+	// In this functions we ask users them names and assign them their shapes.
+	void Acquaintance();
+
+	// This function will rip the field to players using a global variable initialized in MainSource
+	void Field—reation();
+
+	// Function that deduces the rules of playing tic-tac-toe
+	void DameRules();
+
+	// The beginning of the game, the players' moves and the entry of certain symbols into the cells
+	void StartGame();
+};
--- TicTacToeGame/MainSource.cpp
+++ TicTacToeGame/MainSource.cpp
@@ -1,11 +1,12 @@
 // * The simplest tic-tac-toe game.
 /// * Written by - 4Keyy. Repository on GitHub - https://github.com/4Keyy/TicTacToeGame
+/// * The code is written in the Visual Studio program, therefore it has third-party files for the program to work correctly in Visual Studio
 // ---------------------------------------------------------------------------------------- //
 // My first project, which was written by myselfand fully commented out.
 // The project was written with practically minimal knowledge of the language, and was created for its practiceand study. As the project is written, the skill of writing the code will change.
 // Tasks to the project: not only a working program, but also the maximum absence of bad code.
 
-// * Ñonnect all the necessary libraries
+// * Connect all the necessary libraries
 #include <iostream> // Standard library for working with C ++ console
 #include <string> // Library for working with string variables
 #include <stdio.h> // Standard library for working with the C console
@@ -15,12 +16,14 @@
 // Connecting the required namespace
 using namespace std;
 
+// Creating global variables
+char[9]{ '-', '-', '-', '-', '-', '-', '-', '-', '-' };
+
 // Main function
 void main()
 {
 	// Write this for normal work with Cyrillic in the console
 	setlocale(0, "");
-
 
 	// Write this to make the main function work nicely.
 	system("pause");
