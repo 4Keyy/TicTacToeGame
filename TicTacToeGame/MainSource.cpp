0ac0dd3e62204e0ccf10ccdf42ff566cb7bba98b
4keyy <4kpoison@gmail.com>
2021-08-02 11:57:18 +0300

    Started writing the mainSource file, the main function is titled, the necessary libraries are spelled out

--- TicTacToeGame/MainSource.cpp
+++ TicTacToeGame/MainSource.cpp
@@ -1,1 +1,28 @@
-#include <iostream>
+// * The simplest tic-tac-toe game.
+/// * Written by - 4Keyy. Repository on GitHub - https://github.com/4Keyy/TicTacToeGame
+// ---------------------------------------------------------------------------------------- //
+// My first project, which was written by myselfand fully commented out.
+// The project was written with practically minimal knowledge of the language, and was created for its practiceand study. As the project is written, the skill of writing the code will change.
+// Tasks to the project: not only a working program, but also the maximum absence of bad code.
+
+// * Ñonnect all the necessary libraries
+#include <iostream> // Standard library for working with C ++ console
+#include <string> // Library for working with string variables
+#include <stdio.h> // Standard library for working with the C console
+#include <Windows.h> // Library for working with Windows OC files
+#include <time.h> // Library for working with time
+
+// Connecting the required namespace
+using namespace std;
+
+// Main function
+void main()
+{
+	// Write this for normal work with Cyrillic in the console
+	setlocale(0, "");
+
+
+	// Write this to make the main function work nicely.
+	system("pause");
+	system("cls");
+}
